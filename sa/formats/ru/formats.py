e4de76147e38f1443f9dc8e60f4fb823c425e32c
Roman Dolgiy <roman@bravetstudio.com>
2011-12-17 14:14:17 +0200

    Initial commit

---
+++ sa/formats/ru/formats.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+DATE_FORMAT = "l, j E Y г."
+TIME_FORMAT = "H:i"
+DATETIME_FORMAT = "l, j E Y г. H:i"
---
+++ sa/static/source/js/app.js.coffee
@@ -0,0 +1,4 @@
+#= require "jquery"
+
+#$(document).ready ->
+#  alert "hello"
