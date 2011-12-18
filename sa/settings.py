74abb83dd9d6a548007d92be2c55d7d2e8bd1fd5
Roman Dolgiy <roman@bravetstudio.com>
2011-12-18 13:19:48 +0200

    Updated static

--- sa/settings.py
+++ sa/settings.py
@@ -68,7 +68,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(ROOT, "static/build")
+STATIC_ROOT = os.path.join(ROOT, "all_static")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -81,9 +81,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(ROOT, "static/build"),
 )
 
 # List of finder classes that know how to find static files in
@@ -133,6 +131,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.admin',
+    'django.contrib.staticfiles',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 
@@ -181,3 +180,8 @@ FIRST_DAY_OF_WEEK = 1  # Monday
 DEBUG_TOOLBAR_CONFIG = {
     'INTERCEPT_REDIRECTS': False,
 }
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
