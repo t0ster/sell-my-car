897d385e6140a5e0c81d85b9c0998207770695cd
Roman Dolgiy <roman@bravetstudio.com>
2011-12-18 13:34:18 +0200

    Updated

---
+++ sa/static/build/js/plugins.js
@@ -0,0 +1,3 @@
+// usage: log('inside coolFunc', this, arguments);
+// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
+window.log=function(){log.history=log.history||[],log.history.push(arguments),this.console&&(arguments.callee=arguments.callee.caller,console.log(Array.prototype.slice.call(arguments)))},function(a){function b(){}for(var c="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),d;d=c.pop();)a[d]=a[d]||b}(window.console=window.console||{})
